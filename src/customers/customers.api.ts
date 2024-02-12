3b9e337b7b771e36bce66a9e69735ab19a0eb4f0
Marco Kuidja <kuidjamarco@gmail.com>
2024-02-12 13:11:21 +0100

    feat: handled NotchPay cusomer fetch `all` and `:id` API

--- src/customers/customers.api.ts
+++ src/customers/customers.api.ts
@@ -1,5 +1,5 @@
 import { AxiosInstance } from 'axios'
-import { CreateCustomerPayload, Customer, CustomerResponse } from './customers'
+import { CreateCustomerPayload, Customer, CustomerResponse, CustomersResponse } from './customers'
 
 export class CustomersApi {
   constructor(private readonly axiosInstance: AxiosInstance) {}
@@ -12,11 +12,24 @@ export class CustomersApi {
     return { ...resp.data, code: Number(resp.data.code) }
   }
 
-
   async update<T extends object>(payload: CreateCustomerPayload<T>) {
     const resp = await this.axiosInstance.put<CustomerResponse<T>>(
       '/customers',
       payload
+    )
+    return { ...resp.data, code: Number(resp.data.code) }
+  }
+
+  async findOne<T extends object>(customerId: string) {
+    const resp = await this.axiosInstance.get<CustomerResponse<T>>(
+      `/customers/${customerId}`
+    )
+    return { ...resp.data, code: Number(resp.data.code) }
+  }
+
+  async findAll<T extends object>(customerId: string) {
+    const resp = await this.axiosInstance.get<CustomersResponse<T>>(
+      `/customers/${customerId}`
     )
     return { ...resp.data, code: Number(resp.data.code) }
   }
